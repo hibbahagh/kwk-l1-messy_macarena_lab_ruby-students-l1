536ec62495f4d43057f7114c6418e5859bdb352a
hibbahagh <hibbahagh@gmail.com>
2018-06-05 19:51:12 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,11 @@
+local_greeting = "hey DUDeee"
+your_name = "Randi"
+my_name = "Ame"
+time_of_day = "Summer time" 
+
+def greeting
+
+  puts "#{local_greeting} #{your_name}, i'm #{my_name}, how's your #{time_of_day}?"
+end
+
+greeting
